030418543535aa2a3b28fb06be73707f6bb83200
jaffar-ux <hawlaadmi@gmail.com>
2025-07-31 10:12:06 +0530

    Update MusicPlayer.jsx

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -19,7 +19,7 @@ export default function MusicPlayer({ musicPlaying }) {
     return (
         <div>
             <audio ref={audioRef} loop>
-                <source src="/audio/bg.mp3" type="audio/mpeg" />
+                <source src="/audio/Bgm new.mp3" type="audio/mpeg" />
             </audio>
         </div>
     )
